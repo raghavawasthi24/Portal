76a8a8ae85a990bdf924525c45b02acd403a3be9
Shipra <shipratripathi.500@gmail.com>
2023-06-26 17:41:47 +0530

    get candidate created

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Login from "./User/pages/Login/Login"
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LeaderBoard from "./Admin/Pages/LeaderBoard/LeaderBoard"
 import GetQuestions from "./Admin/Pages/Questions/GetQuestions/GetQuestions";
+import GetCandidates from "./Admin/Pages/Candidates/GetCandidates/GetCandidates";
 
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
         <Route exact path="test" element={<Test />}></Route>
         <Route exact path="leaderboard" element={<LeaderBoard/>}/>
         <Route exact path="getQuestion" element={<GetQuestions/>}/>
+        <Route exact path="getCandidate" element={<GetCandidates/>}/>
       </Routes>
     </BrowserRouter>
   );
