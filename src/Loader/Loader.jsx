d2fdb67e907633a454d0782423fb8359f4e8b975
pushkar-28 <kharepushkar2804@gmail.com>
2023-08-16 17:13:28 +0530

    loader img import

--- src/Loader/Loader.jsx
+++ src/Loader/Loader.jsx
@@ -1,21 +1,16 @@
-import React from 'react';
+import React from "react";
 import "./Loader.css";
-import frame1 from "../../public/Images/Frame1.svg";
-import frame2 from "../../public/Images/Frame2.svg";
-import frame3 from "../../public/Images/Frame3.svg";
 
 const Loader = () => {
   return (
-    <div className='cont'>
-
-       <div className='small'>
-           <img src={frame1} alt="" className='frame1'/>
-           <img src={frame2} alt="" className='frame2'/>
-           <img src={frame3} alt="" className='frame3'/>
-       </div>
-
+    <div className="cont">
+      <div className="small">
+        <img src={"../../public/Images/Frame1.svg"} alt="" className="frame1" />
+        <img src={"../../public/Images/Frame2.svg"} alt="" className="frame2" />
+        <img src={"../../public/Images/Frame3.svg"} alt="" className="frame3" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Loader
+export default Loader;
