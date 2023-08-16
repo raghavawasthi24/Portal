e7ae5b796ec19b40e149d6d5495c18b50b76b4dc
pushkar-28 <kharepushkar2804@gmail.com>
2023-08-17 01:08:30 +0530

    results and thankyou

--- src/User/pages/TestResult/TestResult.jsx
+++ src/User/pages/TestResult/TestResult.jsx
@@ -1,47 +1,113 @@
-import { Button, Typography } from '@mui/material'
-import { Box,Container } from '@mui/system'
-import './TestResult.css'
-import { useNavigate } from 'react-router-dom'
-
+import React from "react";
+import {
+  Button,
+  Typography,
+  Table,
+  TableContainer,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@mui/material";
+import { Container } from "@mui/system";
+import "./TestResult.css";
+import { useNavigate } from "react-router-dom";
 
 const TestResult = () => {
-    const navigate =useNavigate()
-    let result = ((JSON.parse(localStorage.getItem("totalScoreStatus"))).user)
-    let totalScore=(((JSON.parse(localStorage.getItem("totalScoreStatus"))).totalscore))
+  const navigate = useNavigate();
+  let result = JSON.parse(localStorage.getItem("totalScoreStatus")).user;
+  let totalScore = JSON.parse(
+    localStorage.getItem("totalScoreStatus")
+  ).totalscore;
+
   return (
     <Container className="resultMain">
-    <Typography className='resultHead'>Test Result</Typography>
-    <Box className="resultFlex">
-    <Box className="Unanswered" id="resultBox">
-        <Typography className='resultSubhead'>Unanswered</Typography>
-        <Typography className='resultValue'>{result.unanswered}</Typography>
-    </Box>
-    <Box className="markedIncorrect" id="resultBox">
-        <Typography className='resultSubhead'>Marked Incorrect</Typography>
-        <Typography className='resultValue'>{result.markedWrong}</Typography>
-    </Box> 
-     <Box className="markedCorrect" id="resultBox">
-        <Typography className='resultSubhead'>Marked Correct</Typography>
-        <Typography className='resultValue'> {result.markedCorrect}</Typography>
-    </Box> 
-     <Box className="correct" id="resultBox">
-        <Typography className='resultSubhead'>Correct</Typography>
-        <Typography className='resultValue'>{result.correct}</Typography>
-    </Box>  
-    <Box className="wrong" id="resultBox">
-        <Typography className='resultSubhead'>Incorrect</Typography>
-        <Typography className='resultValue'>{result.wrong}</Typography>
-    </Box>
-    </Box>
-    <Box className="resultFlex">
-    <Box className="total" >
-    <Typography className='totalscore'>Total Score</Typography>
-        <Typography className='totalscore'>{totalScore}</Typography>
-    </Box>
-    </Box>
-    <button className='resultBtn' onClick={()=>navigate('/feedback')}>Continue to Feedback</button>
+      <Typography className="resultHead">Test Result</Typography>
+      <TableContainer
+        component={Paper}
+        style={{ width: "50%", margin: "auto" }}
+      >
+        <Table>
+          <TableHead>
+            <TableRow className="!bg-reviewColor">
+              <TableCell
+                style={{ border: "1px solid #999" }}
+                className=" !text-white"
+              >
+                Category
+              </TableCell>
+              <TableCell
+                style={{ border: "1px solid #999" }}
+                className=" !text-white"
+              >
+                Value
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell style={{ border: "1px solid #999" }}>
+                Unanswered
+              </TableCell>
+              <TableCell style={{ border: "1px solid #999" }}>
+                {result.unanswered}
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell style={{ border: "1px solid #999" }}>
+                Marked & Incorrect
+              </TableCell>
+              <TableCell style={{ border: "1px solid #999" }}>
+                {result.markedWrong}
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell style={{ border: "1px solid #999" }}>
+                Incorrect
+              </TableCell>
+              <TableCell style={{ border: "1px solid #999" }}>
+                {result.wrong}
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell style={{ border: "1px solid #999" }}>
+                Marked & Correct
+              </TableCell>
+              <TableCell style={{ border: "1px solid #999" }}>
+                {result.markedCorrect}
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell style={{ border: "1px solid #999" }}>
+                Correct
+              </TableCell>
+              <TableCell style={{ border: "1px solid #999" }}>
+                {result.correct}
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell style={{ border: "1px solid #999" }}>
+                Total Score
+              </TableCell>
+              <TableCell style={{ border: "1px solid #999" }}>
+                {totalScore}
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Button
+        className="resultBtn"
+        onClick={() => navigate("/feedback")}
+        variant="contained"
+        color="primary"
+        sx={{ mt: 3 }}
+      >
+        Continue to Feedback
+      </Button>
     </Container>
-  )
-}
+  );
+};
 
 export default TestResult;
