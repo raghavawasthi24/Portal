86cf2b438208c75d97c6e107cc433f0595033044
Raghav Awasthi <raghavawasthi240@gmail.com>
2023-08-24 20:00:18 +0530

    change

--- src/Admin/Pages/Questions/GetQuestions/components/QuesControl.jsx
+++ src/Admin/Pages/Questions/GetQuestions/components/QuesControl.jsx
@@ -4,7 +4,7 @@ import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 import {useSelector,useDispatch} from "react-redux";
 
-import { prevQues,nextQues } from '../../../../../store/slices/PrevNextSlice';
+import { prevQues,nextQues } from '../../../../../store/slices/PrevnextSlice';
 import { Button } from '@mui/material';
 import { quesList } from '../../../../../store/slices/QuestionsSlice';
 
