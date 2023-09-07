fc33d413bb64e8a88c946debfca9ebe86f1d62a6
pushkar-28 <kharepushkar2804@gmail.com>
2023-09-07 14:06:55 +0530

    visited status function modification form cpp

--- src/User/utils/visitedStatus.js
+++ src/User/utils/visitedStatus.js
@@ -2,12 +2,17 @@ import axios from "axios";
 
 const VisitedStatus = async ({ category, quesId }) => {
   const id = localStorage.getItem("id");
-
+  const sendData = {
+    quesId: quesId.includes("C++") ? quesId.replace("C++", "Cpp") : quesId,
+    category: category.includes("C++")
+      ? category.replace("C++", "Cpp")
+      : category,
+  };
   try {
     const res = await axios.get(
-      `${
-        import.meta.env.VITE_APP_NODE_URL
-      }/isVisited/user/${id}?quesId=${quesId}?&category=${category}`
+      `${import.meta.env.VITE_APP_NODE_URL}/isVisited/user/${id}?quesId=${
+        sendData.quesId
+      }?&category=${sendData.category}`
     );
     return res.data;
   } catch (err) {
