7b3ddcc6c1a8a5b9a232408d01f05ba1ec4c6dcf
pushkar-28 <kharepushkar2804@gmail.com>
2023-09-16 02:05:03 +0530

    error loading eager

--- src/Error/Error.jsx
+++ src/Error/Error.jsx
@@ -4,7 +4,7 @@ import error404 from "./assets/404.png";
 const Error = () => {
   return (
     <div className="md:w-2/3 w-full h-screen flex justify-center items-center mx-auto">
-      <img src={error404} alt="error" className="w-full" />
+      <img src={error404} alt="error" className="w-full" loading="eager" />
     </div>
   );
 };
