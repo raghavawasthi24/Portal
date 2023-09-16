1b6f8105adc95f93926839620ee50b3198287180
Raghav Awasthi <raghavawasthi240@gmail.com>
2023-09-16 09:43:04 +0530

    instruction page points change

--- src/User/constants/InstructionContent.jsx
+++ src/User/constants/InstructionContent.jsx
@@ -1,27 +1,32 @@
 const arr=[
     {
         id:1,
-        point:"Every student should check the exam date-sheet and time (session) of the examination."
+        point:" There will be total 5 sections- HTML, SQL,CSS, Aptitude and one Language of your choice."
     },
     {
         id:2,
-        point:" Any student who is late by more than 30 minutes after commencement of examination shall not be permitted to enter the examination hall."
+        point:"Tab switching is not allowed during the quiz. Any attempt to switch tabs, open new windows, or use external resources will result in immediate disqualification. Keep only the quiz tab open during the entire duration of the test. All other tabs must be closed."
     },
     {
         id:3,
-        point:" All the students should carry a valid College Identity card and Admit Card without which they shall not be permitted to write the examination."
+        point:" In case of any technical issues during the quiz (e.g., internet disconnection), please contact the coordinators in the lab immediately if there is a way to do so without violating the no-tab-switching rule. The organizers will provide guidance on how to proceed."
     },
     {
         id:4,
-        point:" No reading material is permitted inside the examination room."
+        point:"The quiz consists of multiple-choice questions (MCQs) and lasts for one hour. Pay close attention to the instructions provided within the quiz platform. Manage your time effectively to answer as many questions as possible within the allotted time. Submit your responses within the given time frame. Any submissions made after the quiz deadline will not be accepted."
     },
     {
         id:5,
-        point:" Use of electronic gadgets (smart phones, smart watches etc.) is strictly   prohibited. Anyone in possession is liable for disciplinary action."
+        point:"You are allowed only one attempt for this quiz. Once you submit the quiz, you will not be allowed to log in again. If you accidentally submit the quiz before completing all questions, you cannot return to the quiz."
     },
     {
         id:6,
-        point:"There will be total 5 sections- HTML, SQL,CSS, Aptitude and one Language of your choice."
-    }
+        point:" Be cautious not to keep any other tab or window open for more than 2 minutes while taking the quiz. If any other tab is open for more than 2 minutes, the quiz may be automatically submitted."
+    },
+    {
+        id:7,
+        point:"Use of electronic gadgets (smart phones, smart watches etc.) is strictly   prohibited. Anyone in possession is liable for disciplinary action."
+    },
+    
 ]
 export default arr;
